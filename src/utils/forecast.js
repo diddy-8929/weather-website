d4759343f6eeb19606b737520ef1c2b37de2f106
Darshan Chawda <darshan_dharmendra.chawda.kt@hitachi.com>
2021-07-09 14:17:08 +0900

    Add humidity parameter to forecast.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,8 @@ const forecast = ((latitude, longitude, callback) => {
         else {
             callback(undefined, body.current.weather_descriptions[0] + 
                     ", It is currently " + body.current.temperature + " degrees out. " +
-                    "It feels like " + body.current.feelslike + " degree out.")
+                    "It feels like " + body.current.feelslike + " degree out " +
+                    "and humidity level is " + body.current.humidity + "%.");
         }
     })
 })
